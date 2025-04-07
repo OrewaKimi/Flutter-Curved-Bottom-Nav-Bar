4861def4cbe0cd7fbdd0c5f83fb33495a27dd499
OrewaKimi <kimi93.com@gmail.com>
2025-04-07 15:18:04 +0700

    add path homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curvedbottomnavbar/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Homepage(),
     );
   }
 }
