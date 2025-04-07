651b3fa227d110436b8776e95643e39c22c81898
OrewaKimi <kimi93.com@gmail.com>
2025-04-07 15:25:26 +0700

    add componenets icons

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -10,6 +11,16 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor:Colors.deepPurple,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.deepPurple,
+        items: [
+          Icon(Icons.home),
+          Icon(Icons.favorite),
+          Icon(Icons.settings),
+        ],
+      ),
+    );
   }
 }
